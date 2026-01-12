5413f2ae02729de88c3b3576fff75436aeccf2c0
Irshad-11 <irshadrisad@gmail.com>
2026-01-13 00:00:39 +0600

    Move secrets to .env file

--- backend/tarikh/settings.py
+++ backend/tarikh/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
@@ -81,11 +83,11 @@ WSGI_APPLICATION = 'tarikh.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tarikh_db',
-        'USER': 'postgres',
-        'PASSWORD': 'irshad',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.environ.get('DB_NAME', 'tarikh_db'),
+        'USER': os.environ.get('DB_USER', 'tarikh_user'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
